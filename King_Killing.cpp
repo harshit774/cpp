8514ea101c5e7c41a3dcd6b10e3d94ccb3abaaf2
Harshit Varshney <harshitvarshney774@gmail.com>
2021-05-17 23:19:35 +0530

    Change and complete

--- King_Killing.cpp
+++ King_Killing.cpp
@@ -13,138 +13,112 @@
 using namespace std;
 const int E = 2e3 + 100;
 const int mod = 1e9 + 7;
+const int mxn = 3e5 + 2;
+vector <int> v[mxn];
+vector <int> level[mxn];
+vector <int> P(mxn , 0);
+vector <int> deg(mxn , 0);
+vector <int> variety(mxn , 0) , LEV(mxn , 0);
+vector <int> subtree(mxn , -1);
+vector <int> dp(mxn , mxn);
+vector <int> dd(mxn);
+vector <int> ans;
+unordered_map <int, int> sub;
+vector<pair<int, int>> curr;
 
-struct TreeNode
+void dfs(int x , int par)
 {
-    int data,level;
-    vector<TreeNode *> child;
-    TreeNode(int x)
-    {
-        data = x;
-        level = 0;
-    }
-};
-void buildTree(TreeNode *root,vector<vector<int>> &adj,vector<bool> &vis)
-{
-    vis[root->data]=1;
-    for(int i=0; i < adj[root->data].size(); i++)
-    {
-        if(!vis[adj[root->data][i]])
-        {
-            TreeNode *tmp = new TreeNode(adj[root->data][i]);
-            root->child.pb(tmp);
+    P[x] = par;
+    for (auto it : v[x]) {
+        if (it != par) {
+            dfs(it , x);
         }
     }
-    for(int i=0; i < root->child.size(); i++)
-    {
-        buildTree(root->child[i] , adj , vis);
+
+    unordered_map <int, int> mp;
+    for (auto it : v[x]) {
+        if (it != par) {
+            dp[x] = min(dp[x] , 1 + dp[it]);
+            mp[dp[it]]++;
+        }
     }
+
+    if (mp.size() > 1 && x != 1)
+        dp[x]++;
+
 }
 
-void dfs(TreeNode *root,int l)
-{
-    root->level = l;
-    for(int i=0; i < root->child.size(); i++)
-    {
-        TreeNode *curr = root->child[i];
-        dfs(curr,l+1);
+void final(int x , int par , int day) {
+
+    dd[x] = day;
+    if (deg[x] == 1) {
+        ans.push_back(x);
+        return;
     }
+
+    for (auto it : v[x]) {
+        if (it != par && dp[it] + 1 >= dd[x]) {
+            final(it , x , min(dp[it] , day));
+        }
+    }
+
 }
 
-vector<pair<int,int>> leaves;
-void dfs2(TreeNode *root)
+void clean(int n)
 {
-    for(int i=0; i < root->child.size(); i++)
-    {
-        TreeNode *curr = root->child[i];
-        dfs2(curr);
+    for (int i = 0 ; i <= n ; ++i) {
+        v[i].clear();
+        level[i].clear();
+        deg[i] = 0;
+        dp[i] = mxn;
     }
-    if(root->child.size() == 0)
-    {
-        leaves.pb({root->level , root->data + 1});
-    }
+
+    ans.clear();
 }
 
-int main()
-{
-ios_base::sync_with_stdio(false);
-cin.tie(NULL);
-ll t;cin>>t;
-while(t--)
-{
-    int n;cin>>n;
-    vector<vector<int>> adj(n);
-    for(int i=0; i < n-1; i++)
-    {
-        int u,v;cin>>u>>v;
-        u-- , v--;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
-    TreeNode *root = new TreeNode(0);
-    vector<bool> vis(n, 0);
-    buildTree(root, adj, vis);
-    dfs(root, 0);
-    vector<pair<int,int>> ans;
-    for(int i=0; i < root->child.size(); i++)
-    {
-        dfs2(root->child[i]);
-        vector<pair<int,int>> v;
-        pair<int,int> p;
-        bool ok = false;
-        sort(leaves.begin(), leaves.end());
-        p = leaves[0];
-        v.pb(p);
-        int val = p.first;
-        int j = 1;
-        while(j < leaves.size())
-        {
-            while(j < leaves.size() and leaves[j].first == val)
-            {
-                if(ok)
-                {
-                    v.pb({p.first + 1, leaves[j].second});
-                }
-                else{
-                    v.pb(leaves[j]);
-                }
-                j++;
-            }
-            if(j < leaves.size())
-            {
-                v.clear();
-                ok = true;
-                p.second = leaves[j].second;
-                v.pb({p.first+1,leaves[j].second});
-                val = leaves[j].first;
-                j++;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t;cin >> t;
+    while (t--) {
+        int n;cin >> n;
+        clean(n);
+        for (int i = 0 ; i < n - 1 ; ++i) {
+            int x, y;
+            cin >> x >> y;
+            v[x].pb(y);
+            v[y].pb(x);
+            deg[x]++;
+            deg[y]++;
+        }
+
+        for (int i = 2 ; i <= n ; ++i)
+            if (deg[i] == 1)
+                dp[i] = 0;
+
+
+        dfs(1 , 0);
+
+        int days = dp[1];
+
+        vector <pair<int, int>> pos;
+        for (auto it : v[1]) {
+            if (dp[it] == days - 1) {
+                final(it , 1 , days - 1);
             }
         }
-        leaves.clear();
-        for(auto x : v)
-        {
-            ans.pb(x);
+
+        sort(ans.begin() , ans.end());
+
+        cout << ans.size() << " " << days << '\n';
+        for (auto it : ans) {
+            cout << it << " ";
         }
+        cout << '\n';
     }
-    sort(ans.begin(), ans.end());
-    int d = ans[0].first;
-    vector<int> aa;
-    for(int i=0; i < ans.size(); i++)
-    {
-        if(ans[i].first == d)
-        {
-            aa.pb(ans[i].second);
-        }
-        else{
-            break;
-        }
-    }
-    cout<< aa.size() << " " << d <<'\n';
-    for(auto x : aa)
-    {
-        cout<< x << " ";
-    }
-    cout<<'\n';
-}
-return 0;
-}
+
+    return 0;
+} // Code Contributed by Harshit Varshney
