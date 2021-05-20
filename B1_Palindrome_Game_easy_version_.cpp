808711320ec9d5995bdd840fa9127eedb2a0b705
Harshit Varshney <harshitvarshney774@gmail.com>
2021-05-20 23:02:48 +0530

    Done

---
+++ B1_Palindrome_Game_easy_version_.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#define ll long long int
+#define pb push_back
+#define fo(i,n) for(int i=0;i<n;i++)
+#define ff(i,n) for(int i=n-1;i>=0;i--)
+#define fff(i,a,b) for(int i = a; i <= b ; i++)
+#define pb push_back
+#define YES cout<< 'YES' << endl 
+#define NO cout<< 'NO' << endl 
+using namespace std;
+const int E = 2e3 + 100;
+const int mod = 1e9 + 7;
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+ll t;cin>>t;
+while(t--)
+{
+    ll n;cin>>n;
+    string s;cin>>s;
+    ll count = 0;
+    fff(i,0,n)
+    {
+        if(s[i]=='1')
+        {
+            count++;
+        }
+    }
+    if(count==n) cout<< "DRAW" <<'\n';
+    // else if(count%4==1 or count%4==2) cout<< "BOB" <<'\n';
+    // else if(count%4==3) cout<< "ALICE" <<'\n';
+    count = n - count;
+    if(count&1)
+    {
+        if(count==1) cout<< "BOB" <<'\n';
+        else cout<< "ALICE" <<'\n';
+    }
+    else cout<< "BOB" <<'\n';
+}
+return 0;
+} //Code Contributed by Harshit Varshney
