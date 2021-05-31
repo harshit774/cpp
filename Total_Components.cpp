e05fe5ae263898863ac4c6e8c04f8faa18c78cda
Harshit Varshney <harshitvarshney774@gmail.com>
2021-05-31 23:08:53 +0530

    Change

--- Total_Components.cpp
+++ Total_Components.cpp
@@ -1,55 +1,3 @@
-// #include<bits/stdc++.h>
-// #define ll long long int
-// using namespace std;
-// const int mod=1e9+7;
-// //The end result of coders personal growth is,there codes becomes there documentation
-// const int MAX = 10000000;
-// long prefix[MAX + 1]; 
-// void pre()
-// {
-//     bool prime[MAX + 1];
-//     memset(prime, true, sizeof(prime));
-//     for (ll p = 2; p * p <= MAX; p++) 
-//     {
-//         if (prime[p]) 
-//         {
-//             for (ll i = p * 2; i <= MAX; i += p)prime[i] = false;
-//         }
-//     }
-//     prefix[0] = prefix[1] = prefix[2]=0;
-//     for (ll p = 3; p <= MAX; p++) 
-//     {
-//         prefix[p] = prefix[p - 1];
-//         if (prime[p])prefix[p]++;
-//     }
-// }
-// ll query(ll L, ll R)
-// {
-//     return (prefix[R] - prefix[L]);
-// }
-// void solve()
-// {
-//     ll N;cin>>N;  //cout<<N<<'\n';
-//     ll l = 1,r=N;
-//     //ll ans=1 + ;
-//     // for(ll i=r;i>=l;--i)
-//     // {
-//     //     if(i&1)
-//     //     {
-//     //         if((i*2)>r)++ans,cout<<i<<' ';
-//     //     }
-//     // 
-//     //if()
-//     cout<< query(N/2,N) + 1 <<'\n';
-// }
-// int main(void)
-// {
-// ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);pre();
-// int T=1;        cin>>T;
-// while(T--){solve();}exit(0);
-// }//Solved By:- Ritik Agarwal/
-
-
 #include<bits/stdc++.h>
 #include<iostream>
 #include<algorithm>
@@ -98,23 +46,21 @@ ll query(ll L, ll R)
     return suffix[R] - suffix[L];
 }
 
-// void accepted()
-// {
-    
-// }
+void accepted()
+{
+    ll n;cin>>n;
+    ll l=1,r=n;
+    cout<< query(n/2,n) + 1 <<'\n';
+}
 
 int main()
 {
 ios_base::sync_with_stdio(false);
-cin.tie(NULL);
+cin.tie(NULL);cout.tie(NULL);sharmile();
 ll t;cin>>t;
 while(t--)
 {
-    ll n;cin>>n;
-    ll l=1,r=n;
-    sharmile();
-    cout<< query(n/2,n) + 1 <<'\n';
-    //accepted();
+    accepted();
 }
 return 0;
 } //Code Contributed by Harshit Varshney
