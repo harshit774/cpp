3dd1aa9e725aa49929cc4d4edc53ec1965613e3e
Harshit Varshney <harshitvarshney774@gmail.com>
2021-06-01 23:11:05 +0530

    Done

---
+++ A_XORinacci.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#define ll long long int
+#define pb push_back
+#define fo(a,b,c) for(int a=b;a<c;a++)
+#define ff(a,b,c) for(int a=b;a>=c;a--)
+#define pb push_back
+#define YES cout<< 'YES' << endl 
+#define NO cout<< 'NO' << endl 
+using namespace std;
+const int E = 2e3 + 100;
+const int mod = 1e9 + 7;
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+ll t;cin>>t;
+while(t--)
+{
+    ll a,b,n;cin>>a>>b>>n;
+    if(n%3==0) cout<< a <<'\n';
+    else if(n%3==1) cout<< b <<'\n';
+    else cout<< (a^b) <<'\n';
+}
+return 0;
+} //Code Contributed by Harshit Varshney
