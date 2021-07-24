400e03dac25178e9ba4b88c9e95725de0d79fd06
Harshit Varshney <harshitvarshney774@gmail.com>
2021-07-24 20:28:36 +0530

    Done

---
+++ A_Polycarp_and_Coins.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<iostream>
+#include<algorithm>
+#include<queue>
+#define ll long long int
+#define pb push_back
+#define fo(a,b,c) for(int a=b;a<c;a++)
+#define ff(a,b,c) for(int a=b;a>=c;a--)
+#define pb push_back
+#define YES cout<< 'YES' << endl 
+#define NO cout<< 'NO' << endl 
+using namespace std;
+const int E = 2e3 + 100;
+const int mod = 1e9 + 7;
+int32_t main()
+{
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);
+ll t;cin>>t;
+while(t--)
+{
+    ll n;cin>>n;
+    ll ans = n/3;
+    ll sol = n - ans;
+    if(ans&1){
+        sol+=1;
+    }
+    cout<< sol/2 << " " << ans <<'\n';
+}
+return 0;
+} //Code Contributed by Harshit Varshney
