2e720fdc1d9808bc7f97122515540cade6256f91
Harshit Varshney <harshitvarshney774@gmail.com>
2021-08-07 10:39:27 +0530

    Done

--- A_Find_The_Array.cpp
+++ A_Find_The_Array.cpp
@@ -1,18 +1,8 @@
 #include<bits/stdc++.h>
-#include<iostream>
-#include<algorithm>
-#include<queue>
 #define ll long long int
-#define pb push_back
-#define fo(a,b,c) for(int a=b;a<c;a++)
-#define ff(a,b,c) for(int a=b;a>=c;a--)
-#define pb push_back
-#define YES cout<< 'YES' << endl 
-#define NO cout<< 'NO' << endl 
 using namespace std;
-const int E = 2e3 + 100;
-const int mod = 1e9 + 7;
-int32_t main()
+
+int main()
 {
 ios_base::sync_with_stdio(false);
 cin.tie(NULL);
